470c154c8e5b591773cab33dc0fcd7521a8f93e1
Patrick Spek <p.spek@tyil.nl>
2016-09-11 11:45:19 +0200

    Add additional aliases

---
+++ git/git-push-all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+main()
+{
+	branch="master"
+
+	if test -n "$1"
+	then
+		branch="$1"
+	fi
+
+	for remote in $(git remote)
+	do
+		echo "Pushing to ${remote}:${branch}..."
+		git push "${remote}" "${branch}"
+	done
+}
+
+main "$@"
+
