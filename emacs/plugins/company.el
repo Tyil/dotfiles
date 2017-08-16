667c6310d4022c14d2604ead51119f5a03b859c6
Patrick Spek <p.spek@tyil.nl>
2017-08-16 08:52:45 +0200

    Use correct package names for use-package

--- emacs/plugins/company.el
+++ emacs/plugins/company.el
@@ -1,2 +1,2 @@
-(use-package company-mode)
+(use-package company)
 (add-hook 'after-init-hook 'global-company-mode)
--- emacs/plugins/smartparens.el
+++ emacs/plugins/smartparens.el
@@ -1,2 +1,2 @@
-(use-package smartparens-config)
+(use-package smartparens)
 (add-hook 'prog-mode-hook #'smartparens-strict-mode)
